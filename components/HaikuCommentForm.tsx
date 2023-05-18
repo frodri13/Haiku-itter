6682186f8ccbb3357b2ef76f6aee5b968f02b4ea
Fabio Rodriguez <alessf13@gmail.com>
2023-05-18 11:43:15 +0100

    haikuCommentForm

---
+++ components/HaikuCommentForm.tsx
@@ -0,0 +1,7 @@
+export default function HaikuCommentForm() {
+    return(
+        <div>
+            Comments
+        </div>
+    )
+}
--- components/HaikuPosts.tsx
+++ components/HaikuPosts.tsx
@@ -1,5 +1,7 @@
+
 import { prisma } from '@/lib/prisma'
 import { ProfileImage } from './ProfileImageProps';
+import { useState } from 'react';
 
 
 export default async function HaikuPosts() {
@@ -12,9 +14,14 @@ export default async function HaikuPosts() {
             const src = post.userImage;
 
             return(
-            <div key={post.id}>
+            <div key={post.id} className='flex gap-2'>
                 <ProfileImage src={src}/>
                 <p className='whitespace-pre-wrap'> {content}</p>
+                <textarea 
+                style={{height: 0}}
+                placeholder='Haiku it?'
+                className='flex-grow resize-none
+                overflow-hidden p-4 text-sm outline-none' />
             </div>
             )
           })}
