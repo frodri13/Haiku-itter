50468993adffbc3aac1f95b6f4ce5dc29e7bedf6
Fabio Rodriguez <alessf13@gmail.com>
2023-05-19 10:53:20 +0100

    comments shown

--- components/HaikuPosts.tsx
+++ components/HaikuPosts.tsx
@@ -6,7 +6,11 @@ import NewHaikuForm from './NewHaikuForm';
 
 
 export default async function HaikuPosts() {
-    const posts = await prisma.post.findMany();
+    const posts = await prisma.post.findMany(
+        {include: {
+            comments: true, // Include the comments related to each post
+          },}
+    );
 
     return(
         <div>
@@ -15,11 +19,22 @@ export default async function HaikuPosts() {
             const src = post.userImage;
             const postID = post.id;
 
-
             return(
             <div key={post.id} className='flex gap-2'>
                 <ProfileImage src={src}/>
                 <p className='whitespace-pre-wrap'> {content}</p>
+                {
+                   post.comments.map((comment: any) => {
+                    const commentContent = JSON.parse(comment.body);
+                    const srcC = comment.userImage;
+
+                    return(<div key={comment.id}>
+                        <ProfileImage src={srcC}/>
+                        
+                        <p className='whitespace-pre-wrap'>{commentContent}</p>
+                    </div>)
+                   }) 
+                    }
                 <NewHaikuForm 
                     buttonText='Haiku it?'
                     placeHolder='Anything to add?'
